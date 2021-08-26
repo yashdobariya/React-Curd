b4f57367d6ec69eb750caf296a95f4e492e55c94
yashdobariya <yashdobariya888@gmail.com>
2021-08-26 15:40:40 +0530

    update textinput

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -3,17 +3,25 @@ import React, { Component } from 'react'
 export default class TextInput extends Component {
     constructor(props){
         super(props);
+        this.state = {value:""}
+
     }
 
+    handleChange = (event) =>{
+        this.setState({value: event.target.value});
+                console.log("call");
+
+    }
+    
     render() {
+                
         return (
             <div className="form-item wh">
                 <label htmlFor="">{this.props.fieldname}</label>
                 <input
                 type="text"
-                className=""
-                name=""
-                id=""
+                value= {this.state.value}
+                onChange={(event) => this.handleChange(event)}     
                 placeholder="Enter company name"
                 />
             </div>
